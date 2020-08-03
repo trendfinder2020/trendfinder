d87bc590f37e8591573a60e0a14ebc4f3cf95bb7
trendfinder2020 <69126644+trendfinder2020@users.noreply.github.com>
2020-08-02 21:06:06 -0300

    Add files via upload

---
+++ service/rest/SystemUserRestService.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * SystemUser REST service
+ */
+class SystemUserRestService extends AdiantiRecordService
+{
+    const DATABASE      = 'permission';
+    const ACTIVE_RECORD = 'SystemUser';
+    const A<?php
+/**
+ * SystemUser REST service
+ */
+class SystemUserRestService extends AdiantiRecordService
+{
+    const DATABASE      = 'permission';
+    const ACTIVE_RECORD = 'SystemUser';
+    const ATTRIBUTES    = ['id', 'name', 'login', 'email', 'system_unit_id', 'active'];
+}
